2345e3e678a33da5708f6e30c5cd4a8613e71b4c
Dave Centeno <centeno.d@northeastern.edu>
2022-07-04 11:55:14 -0400

    Completed tutorial but animated graphs are not working due to typescript updates. Will return and complete once issue is resolved

--- App.js
+++ App.js
@@ -1,12 +1,87 @@
+import React,{ useRef, useMemo, useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View, SafeAreaView, } from 'react-native';
+import { BottomSheetModal, BottomSheetModalProvider } from '@gorhom/bottom-sheet';
+import ListItem from './components/ListItem';
+import { SAMPLE_DATA } from './assets/data/sampleData'; 
+import Chart from './components/Chart';
+import { getMarketData } from './services/cryptoservice';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+const ListHeader = () => (
+  <>
+    <View style={styles.titleWrapper}>
+      <Text style={styles.largeTitle}>Markets</Text>
+      <View style={styles.divider}></View>
       <StatusBar style="auto" />
     </View>
+  </>
+)
+
+export default function App() {
+  //makes a call to sampledata for coin values
+  const [selectedCoinData, setSelectedCoinData] = useState(null)
+  //coingecko api call to coin values 
+  const [data, setData] = useState([]);
+  // ref
+  const bottomSheetModalRef = useRef(null);
+  // variables
+  const snapPoints = useMemo(() => ['50%'], []);
+
+  
+  useEffect(() => {
+    const fetchMarketData = async () =>{
+      const marketData  = await getMarketData();
+      setData(marketData);
+    }
+
+    fetchMarketData();
+
+  }, [])
+
+  const openModal = (item) => {
+    setSelectedCoinData(item);
+    bottomSheetModalRef.current.present();
+  }
+
+  return (
+    <BottomSheetModalProvider>
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          keyExtractor={(item) => item.id}
+          data={data}
+          renderItem={({ item }) => (
+            <ListItem 
+              name={item.name} 
+              symbol={item.symbol} 
+              currentPrice={item.current_price} 
+              priceChangePercentage7d={item.price_change_percentage_7d_in_currency} 
+              logoUrl={item.image}
+              onPress={() => openModal(item)}
+              />
+          )}
+          ListHeaderComponent={<ListHeader/>}
+        />
+      </SafeAreaView>
+      
+      <BottomSheetModal
+      ref={bottomSheetModalRef}
+      index={0}
+      snapPoints={snapPoints}
+      style= {styles.bottomSheet}
+      >
+        {selectedCoinData ? (
+          <Chart 
+          currentPrice={selectedCoinData.current_price}
+          logoUrl={selectedCoinData.image}
+          name={selectedCoinData.name}
+          symbol={selectedCoinData.symbol}
+          priceChangePercentage7d={selectedCoinData.price_change_percentage_7d_in_currency}
+          sparkline={selectedCoinData.sparkline_in_7d.price}
+          />
+          )
+        : null}
+      </BottomSheetModal>
+    </BottomSheetModalProvider>
   );
 }
 
@@ -14,7 +89,30 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+
+  titleWrapper:{
+    marginTop: 20,
+    paddingHorizontal: 16,
+  },
+  largeTitle:{
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  divider:{
+    height: StyleSheet.hairlineWidth,
+    backgroundColor: '#A9ABB1',
+    marginHorizontal: 16,
+    marginTop: 16,
+  },
+  bottomSheet:{
+    shadowColor: "000",
+    shadowOffset: {
+      width: 0,
+      height: -4
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
   },
 });
